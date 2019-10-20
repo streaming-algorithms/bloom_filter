ecedd5ec319b0d8b3dcca36dfc8af0d67714d527
mazghal <aminezghal@vente-privee.com>
2019-10-20 12:18:15 +0200

    set logging basic config

--- src/bloom_filter/__init__.py
+++ src/bloom_filter/__init__.py
@@ -1,1 +1,6 @@
+import logging
+
 from bloom_filter.bloom_filter import BloomFilter
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
